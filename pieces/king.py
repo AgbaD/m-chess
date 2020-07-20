3f150991dc056ec6714d62e774dee824895ab7ed
BlankGodd <blankgodd33@gmail.com>
2020-07-20 16:52:51 +0100

    moving the king

--- pieces/king.py
+++ pieces/king.py
@@ -25,22 +25,26 @@ class King:
 				return cond
 		else:
 			x = self.board[a][b]
-				if player == 1:
-					if x[-1] == " ":
-						return False
-					cond = self.white_take(ab,player)
-					return cond
-				else:
-					if x[-1] == ".":
-						return False
-					cond = self.black_take(ab,player)
-					return cond
+			if player == 1:
+				if x[-1] == " ":
+					return False
+				cond = self.white_take(ab,player)
+				return cond
+			else:
+				if x[-1] == ".":
+					return False
+				cond = self.black_take(ab,player)
+				return cond
 
 	def move_black(self, ab, player):
 		cond = self.check_round(ab, player)
+		if not cond:
+			return cond
+		
 
 	def check_round(self,ab,player):
 		check = ""
+		prev_pos = ""
 		if player == 1:
 			check = "K "
 		else:
@@ -48,10 +52,42 @@ class King:
 		cond = False
 		x,y = ab[0],ab[1]
 		y = int(y)-1
-		pos = x+(str(y))
+		pos = x+str(y)
 		a,b = self.position[pos]
-		if self.board[a][b] = check:
+		if self.board[a][b] == check:
 			cond = True
+			prev_pos = pos
+		if not cond:
+			x,y = ab[0],ab[1]
+			y = int(y)+1
+			pos = x+str(y)
+			a,b = self.position[pos]
+			if self.board[a][b] == check:
+				cond = True
+				prev_pos = pos
+		if not cond:
+			x,y = ab[0],ab[1]
+			n = self.alpha[x]
+			n = n-1
+			x = self.alpha[n]
+			pos = x+y
+			a,b = self.position[pos]
+			if self.board[a][b] == check:
+				cond = True
+				prev_pos = pos
+		if not cond:
+			x,y = ab[0],ab[1]
+			n = self.alpha[x]
+			n = n+1
+			x = self.alpha[n]
+			pos = x+y
+			a,b = self.position[pos]
+			if self.board[a][b] == check:
+				cond = True
+				prev_pos = pos
 		
+		if cond:
+			return prev_pos
+		return cond
 		
 
